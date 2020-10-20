a48cdcb0ae495e1d28ac8b2f71fdc7fe23e10d4f
thurman-IFSC <54566754+thurman-IFSC@users.noreply.github.com>
2020-10-20 15:56:19 -0300

    Add files via upload

---
+++ esboco.py
@@ -0,0 +1,7 @@
+"raiz=[[diretorio],[arquivo],...]"
+"funcao armazenar->armazenar_arquivo"
+"funcao ls"
+"criar uma variavel que contém o endereco do diretorio, ou arquivo, exemplo arquivox=raiz[diretorio][arquivox]"
+"def criar_diretorio(diretorio_que_contera_o_alvo,erro_da_funcao):"
+"def criar_arquivo(diretorio_que_contera_o_alvo,erro_da_funcao): -> parecido cat do unix"
+def ls("diretorio")
