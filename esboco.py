61c8b1e27f847d3c1d9dfcaef61b8f387788a45a
thurman-IFSC <54566754+thurman-IFSC@users.noreply.github.com>
2020-10-20 16:34:03 -0300

    Update esboco.py

--- esboco.py
+++ esboco.py
@@ -1,7 +1,9 @@
-"raiz=[[diretorio],[arquivo],...]"
-"funcao armazenar->armazenar_arquivo"
-"funcao ls"
-"criar uma variavel que contém o endereco do diretorio, ou arquivo, exemplo arquivox=raiz[diretorio][arquivox]"
-"def criar_diretorio(diretorio_que_contera_o_alvo,erro_da_funcao):"
-"def criar_arquivo(diretorio_que_contera_o_alvo,erro_da_funcao): -> parecido cat do unix"
+"""
+raiz=[[diretorio],[arquivo],...]
+funcao armazenar->armazenar_arquivo
+funcao ls"
+criar uma variavel que contém o endereco do diretorio, ou arquivo, exemplo arquivox=raiz[diretorio][arquivox]
+def criar_diretorio(diretorio_que_contera_o_alvo,erro_da_funcao):
+def criar_arquivo(diretorio_que_contera_o_alvo,erro_da_funcao): -> parecido cat do unix
 def ls("diretorio")
+"""
